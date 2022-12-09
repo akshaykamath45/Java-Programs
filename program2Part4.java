1ec8cbcd21aca4fa1d97642f910e5beb4467b72b
Akshay Kamath <73344382+akshaykamath45@users.noreply.github.com>
2022-12-09 18:21:36 +0530

    Default Value of Primitive Data Types
    WAP to display default value of primitive data types

---
+++ program2Part4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    //Data Types to be declared above void main function,not inside it
+    static int a ;
+    static double b ;
+    static float c;
+    static byte d ;
+    static short e ;
+    static long f;
+    static boolean g;
+    static char h;
+    public static void main(String z[]) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Default value of Integer = " + a);
+    System.out.println("Default value of Double = " + b);
+    System.out.println("Default value of Float = " + c);
+    System.out.println("Default value of Byte = " + d);
+    System.out.println("Default value of Short = " + e);
+    System.out.println("Default value of Long = " + f);
+    System.out.println("Default value of Boolean = " + g);
+    System.out.println("Default value of Char = " + h);
+     scan.close();
+    }
+}
+
